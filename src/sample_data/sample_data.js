ac87aab9a18c216daf4af307328bee310da2ad34
kevinegstorf <kevin.egstorf@gmail.com>
2016-02-10 15:31:28 +0100

    added sample data

---
+++ src/sample_data/sample_data.js
@@ -0,0 +1,42 @@
+// This is just some sample data so you don't have to think of your own!
+module.exports = {
+  fish1 : {
+    name : 'Pacific Halibut',
+    price_1 : 1724,
+    price_2 : 1730,
+    price_3 : 1734,
+    price_4 : 1760
+  },
+
+  fish2 : {
+    name : 'Lobster',
+    price_1 : 3200,
+    price_2 : 3250,
+    price_3 : 3251,
+    price_4 : 3260
+  },
+
+  fish3 : {
+    name : 'Sea Scallops',
+    price_1 : 1684,
+    price_2 : 1701,
+    price_3 : 1732,
+    price_4 : 1780
+  },
+
+  fish4 : {
+    name : 'Mahi Mahi',
+    price_1 : 1129 ,
+    price_2 : 1130,
+    price_3 : 1122,
+    price_4 : 80
+  },
+
+  fish5 : {
+    name : 'King Crab',
+    price_1 : 4234,
+    price_2 : 3250,
+    price_3 : 7500,
+    price_4 : 100000
+  }
+};
